3fb98a0635cb2202f39f608973d3007b1758f6c6
JaviBJ99 <javier.martinez127@gmail.com>
2020-07-08 19:25:30 +0200

    Introducción de la caja

---
+++ EscenarioBateria/Escenario/Assets/Scripts/SnareScripts/DetectSound.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectSound : MonoBehaviour
+{
+
+    public SoundController soundcont;
+
+    float TimeHitHoop;
+    float TimeHitSnare;
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+
+            Debug.Log(contact.point.x);
+            if (contact.point.x < -2.7 || contact.point.x > -1.6)
+            {
+                //soundcont.HoopDetectSound();
+                TimeHitHoop = Time.deltaTime;
+                Debug.Log(TimeHitHoop);
+
+            }
+
+            if (contact.point.x > -2.7 && contact.point.x < -1.6)
+            {
+                //soundcont.SnareDetectSound();
+                TimeHitSnare = Time.deltaTime;
+                Debug.Log(TimeHitSnare);
+            }
+
+        }
+
+        if (TimeHitHoop > 0.0f && TimeHitSnare > 0.0f && (TimeHitSnare - TimeHitHoop <= Mathf.Abs(0.07f)))
+        {
+            Debug.Log("EHHHHH");
+            soundcont.audioSource.PlayOneShot(soundcont.rimshot);
+            TimeHitHoop = 0.0f;
+            TimeHitSnare = 0.0f;
+        }
+
+        else if (TimeHitSnare > 0 && TimeHitHoop == 0)
+        {
+            Debug.Log("ARO");
+            soundcont.audioSource.PlayOneShot(soundcont.snare);
+            TimeHitHoop = 0.0f;
+            TimeHitSnare = 0.0f;
+        }
+
+        else if (TimeHitSnare == 0 && TimeHitHoop > 0)
+        {
+            Debug.Log("CAJA");
+            soundcont.audioSource.PlayOneShot(soundcont.hoop);
+            TimeHitHoop = 0.0f;
+            TimeHitSnare = 0.0f;
+        }
+
+
+    }
+}
