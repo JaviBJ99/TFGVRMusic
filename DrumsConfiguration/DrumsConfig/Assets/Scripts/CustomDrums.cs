e3c23db054cb27b16ac81bac0606cc1c2e0b7a48
JaviBJ99 <javier.martinez127@gmail.com>
2020-09-13 19:58:25 +0200

    Menú selección batería

---
+++ DrumsConfiguration/DrumsConfig/Assets/Scripts/CustomDrums.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomDrums : MonoBehaviour
+{
+
+    public int toms;
+
+    public int crashes;
+
+    public int ftoms;
+
+
+
+    public void changeToms(float n)
+    {
+        toms = (int)n;
+    }
+
+    public void changeFToms(float f)
+    {
+        ftoms = (int)f;
+
+    }
+
+    public void changeCrasesh(float c)
+    {
+        crashes = (int)c;
+    }
+
+    
+}
